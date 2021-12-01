234bb249d66e71fdecfa844375aa105e9755aa8d
Bryan García <bryan1997st@gmail.com>
2021-12-01 03:36:49 -0500

    Added conditional rendering

--- src/js/component/Main.jsx
+++ src/js/component/Main.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext.js";
 import { Navbar } from "./navbar/NavBar.jsx";
+import { Charts } from "./search-results/Charts.jsx";
 import { SearchFirstResult } from "./search-results/SearchFirstResult.jsx";
 
 export const Main = () => {
+	const { store, actions } = useContext(Context);
 	return (
 		<div className="main-box">
 			<Navbar />
-			<SearchFirstResult />
+			{store.search.results ? <SearchFirstResult /> : <Charts />}
 		</div>
 	);
 };
