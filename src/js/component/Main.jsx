cf8f7a54326cf6cfc3567b767da14a24d1bad6b4
Bryan García <bryan1997st@gmail.com>
2021-12-02 15:19:52 -0500

    Added music result

--- src/js/component/Main.jsx
+++ src/js/component/Main.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext.js";
+import { Music } from "./music/Music.jsx";
 import { Navbar } from "./navbar/NavBar.jsx";
 import { Charts } from "./search-results/Charts.jsx";
 import { SearchFirstResult } from "./search-results/SearchFirstResult.jsx";
@@ -9,7 +10,22 @@ export const Main = () => {
 	return (
 		<div className="main-box">
 			<Navbar />
-			{store.search.results ? <SearchFirstResult /> : <Charts />}
+			{store.search.results ? (
+				<React.Fragment>
+					<SearchFirstResult />
+					<div className="results-box">
+						<h2 className="results__title">Resultados</h2>
+						<div className="results__music">
+							{store.search.results.map((result, index) => {
+								if (index > 9) return;
+								return <Music data={result} imgUrl={result.album.cover_medium} key={result.id} />;
+							})}
+						</div>
+					</div>
+				</React.Fragment>
+			) : (
+				<Charts />
+			)}
 		</div>
 	);
 };
