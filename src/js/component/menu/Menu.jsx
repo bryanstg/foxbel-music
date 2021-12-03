195781a9be1d4bb677aa531e3ef5315cfd92d1c3
Bryan García <bryan1997st@gmail.com>
2021-12-02 23:23:22 -0500

    Changed Menu folder

---
+++ src/js/component/menu/Menu.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import FoxbelLogo from "./../../../img/foxbel-music@2x.png";
+
+export const Menu = () => {
+	const [active, setActive] = useState({
+		recientes: true
+	});
+
+	const handleActive = link => {
+		setActive({
+			recientes: true,
+			artistas: false,
+			albums: false,
+			canciones: false,
+			estaciones: false
+		});
+	};
+	const windowSize = window.innerWidth;
+	return (
+		<React.Fragment>
+			<div className={`menu__hamburguer ${windowSize > 670 ? "close" : ""}`}>
+				<div className="menu__hamburguer--line" />
+				<div className="menu__hamburguer--line" />
+				<div className="menu__hamburguer--line" />
+			</div>
+			<div className="menu-box">
+				<div className="menu__logo" style={{ backgroundImage: `url(${FoxbelLogo})` }}>
+					{""}
+				</div>
+				<div className="menu__section">
+					<h2 className="section__title" to="/home">
+						{"Mi Libreria"}
+					</h2>
+					<Link className={`section__link ${active.recientes && "active"}`} to="/home">
+						{"Recientes"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Artistas"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Albums"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Canciones"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Estaciones"}
+					</Link>
+				</div>
+				<div className="menu__section">
+					<h2 className="section__title" to="/home">
+						{"Playlist"}
+					</h2>
+					<Link className="section__link" to="/home">
+						{"Metal"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Para Bailar"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Rock 90's"}
+					</Link>
+					<Link className="section__link" to="/home">
+						{"Baladas"}
+					</Link>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
